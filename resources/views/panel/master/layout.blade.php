8698ba43fbc0619d8fb73a420f5e4718637c2018
Mohammad Vatandoost <mohamadvatandoost512@gmail.com>
2019-02-06 07:14:31 -0800

    asdaasd

--- resources/views/panel/master/layout.blade.php
+++ resources/views/panel/master/layout.blade.php
@@ -39,26 +39,23 @@
     </div>
 </header>
 
-@yield('content')
-
-
-
-
-
-
-<!-- Footer -->
-<div id="footer-div">
-
-    <div class="footer-div">
+<div>
+  <div>
+   @yield('content')
+   <!-- Footer -->
+    <div id="footer-div">
+      <div class="footer-div">
         <p class="footer-paragraph">© 2018 HashBazaar. All rights reserved</p>
-        <img id="footer-image" src="{{URL::asset('img/Logo_footer.svg')}}" alt=""></div>
+        <img id="footer-image" src="{{URL::asset('img/Logo_footer.svg')}}" alt="">
+        </div>
 
-</div>
+     </div>
 
-<!-- Container -->
-<div class="mainContainer">
+  </div>
 
-
+  <div>
+   <!-- Container -->
+   <div class="mainContainer">
     <nav class="container">
         <ul class="mainList">
             <li class="navbar"> <a href="http://hashbazaar.com"><img class="Logo_In_NavBar" src="{{URL::asset('img/Logo_In_NavBar.svg')}}" alt="Logo_In_NavBar"></a>
@@ -68,16 +65,19 @@
             <li class="sub"> <a href="{{route('referral')}}" id="referral">Referral</a> </li>
             <li class="sub"> <a href="{{route('setting')}}" id="setting">Setting</a></li>
             <li class="sub"> <a href="{{route('contact')}}" id="contact">Contact</a></li>
-
         </ul>
-
-
-
     </nav>
-
-
-
+   </div> 
+     
+  </div>
 
 </div>
+
+
+
+
+
+
+
 </body>
 </html>

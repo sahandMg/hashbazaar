36f4f4e83ad275b5bb90c4eb1a357fa347fc078c
Mohammad Vatandoost <mohamadvatandoost512@gmail.com>
2019-02-16 07:59:46 -0800

    fix header button

--- resources/views/master/layout.blade.php
+++ resources/views/master/layout.blade.php
@@ -68,7 +68,11 @@
 @yield('content')
 
 
-
+    <script type="text/javascript">
+          window.$crisp=[];window.CRISP_WEBSITE_ID="792f282f-edde-46b8-8b02-d38ca5cb92c2";
+        (function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";
+            s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+     </script>
 
 </body>
 </html>

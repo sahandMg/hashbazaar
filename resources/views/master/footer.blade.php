aa5bcc19f8ae73a0e0279b7878e8211b00e0c467
Alireza Amini <amini_css@yahoo.com>
2019-02-23 19:38:43 +0330

    icons setting

--- resources/views/master/footer.blade.php
+++ resources/views/master/footer.blade.php
@@ -3,18 +3,19 @@
         <p>© 2018 HashBazaar. All rights reserved</p>
     </div>
     <div class="network-flex">
-{{-- 
-        	                <a href="https://facebook.com" class="socialnet-flex" id="fb">
-							    <img src="img/icons/facebook.svg" alt=""></a>
-						
-						    <a href="https://twitter.com" class="socialnet-flex" id="twttr">
-							    <img src="img/icons/twitter.svg" alt=""></a>
-						
 
-						    <a href="#" class="socialnet-flex" id="gp">
-                                <img src="img/icons/googleicon.png" alt=""></a> --}}
+        	                <a href="https://facebook.com" class="socialnet-flex" id="fb" style="z-index:-5">
+                                <img src="img/icons/face.svg" alt=""></a>
                                 
-                                <a href="#" class="socialnet-flex" id="insta">
-                                    <img src="img/icons/insta.jpg" alt=""></a>    
+                            <a href="#" class="socialnet-flex" id="insta">
+                                <img src="img/icons/insta.svg" alt=""></a> 
+      
+						    <a href="https://twitter.com" class="socialnet-flex" id="twttr" style="z-index:-5">
+                                <img src="img/icons/tweet.svg" alt=""></a>
+                                
+						    
+						    {{-- Baraye Zaherkardane icona Z-index Check Beshe --}}
+                                
+                                   
     </div>
 </footer>

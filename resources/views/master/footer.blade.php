f74c919e22ef415c9e88aa3e392a5d8af0bf5676
Alireza Amini <amini_css@yahoo.com>
2019-04-24 13:57:53 +0430

    footer

--- resources/views/master/footer.blade.php
+++ resources/views/master/footer.blade.php
@@ -4,13 +4,13 @@
     </div>
     <div class="network-flex">
         <ul>
-        	                <li><a href="https://www.linkedin.com/company/hashbazaar" class="socialnet-flex" id="fb" >
+        	                <li><a href="https://www.linkedin.com/company/hashbazaar" class="socialnet-flex">
                                 <img src="img/icons/linkedin.png" alt="linkedin"></a></li> 
 
-                            <li> <a href="https://www.instagram.com/hashbazaar/" target="_blank" class="socialnet-flex" id="insta">
+                            <li> <a href="https://www.instagram.com/hashbazaar/" target="_blank" class="socialnet-flex">
                                 <img src="img/icons/insta.svg" alt="hashbazaar instagram"></a> </li>
       
-						    <li><a href="https://twitter.com/Hashbazaar_CMC" class="socialnet-flex" id="twttr">
+						    <li><a href="https://twitter.com/Hashbazaar_CMC" class="socialnet-flex" >
                                 <img src="img/icons/tweet.svg" alt=""></a></li>
         </ul>
     </div>   

2a0b188811eca48b50f9cbba3060f770e6f89a60
Mohammad Vatandoost <mohamadvatandoost512@gmail.com>
2019-05-08 05:42:27 -0700

    add discription image for index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -59,7 +59,8 @@
         </div>
     </section>
   @if(Config::get('app.locale') == 'fa')
-  <section class="context-section backgroundGrey text-center pt-4 pb-4 advantages" style="color: #707070"  >
+  <section class="context-section backgroundGrey text-center pt-4 pb-4 advantages" style="color: #707070;padding: 2%;"  >
+    <img id="price" alt="electricity price" src="img/workflow.jpg" style="width: 100%;height: auto;">
   </section>
   @else
     <section class="context-section backgroundGrey text-center pt-4 pb-4 advantages" style="color: #707070"  >

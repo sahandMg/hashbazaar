d7e2088e4a83fe95fd2673f101721da536a94b38
Mohammad Vatandoost <mohamadvatandoost512@gmail.com>
2019-08-26 11:31:22 -0700

    new dashboard

--- resources/views/remote/minersStatus.blade.php
+++ resources/views/remote/minersStatus.blade.php
@@ -1,26 +1,92 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+@extends('remote.layout')
+@section('content')
 
-    <!-- <link rel="stylesheet" href="{{URL::asset('bootstrap/css/bootstrap.min.css')}}"> -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-
-    <title>Miners Status</title>
-
-</head>
-<body>
-
-<div class="container">
     @if(is_null($minerData))
+      <div class="container">
         <h1> No Data Available</h1>
+      </div>
     @else
-        <h2>{{\Morilog\Jalali\Jalalian::fromCarbon(\Carbon\Carbon::parse($minerData->created_at))}}</h2>
-        <br/>
-         <div class="table-responsive">
+        <!-- <br/> -->
+<div class="row">
+                            <div class="col-lg-9">
+                                <h2>{{\Morilog\Jalali\Jalalian::fromCarbon(\Carbon\Carbon::parse($minerData->created_at))}}</h2>
+                                <h2 class="title-1 m-b-25">ضعیت دستگاه های ماینر</h2>
+                                <div class="table-responsive table--no-card m-b-40">
+                                    <table class="table table-borderless table-striped table-earning">
+                                        <thead>
+                                            <tr>
+                                                <th>Ip</th>
+                                                <th>Type</th>
+                                                <th>Temp2</th>
+                                                <th class="text-right">Temp1</th>
+                                                <th class="text-right">Fan Speed</th>
+                                                <th class="text-right">Total Th</th>
+                                                <th class="text-right">Up time</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                             @for($i=0;$i< count(unserialize($minerData->data));$i++)
+             <tr>
+                <td>{{unserialize($minerData->data)[$i]['ip']}}</td>
+                <td>{{unserialize($minerData->data)[$i]['minerName']}}</td>
+                <td>{{implode( ", ", unserialize($minerData->data)[$i]['temp2'])}}</td>
+                <td>{{implode( ", ", unserialize($minerData->data)[$i]['temp1'])}}</td>
+                <td>{{implode( ", ", unserialize($minerData->data)[$i]['fanSpeeds'])}}</td>
+                <td>{{unserialize($minerData->data)[$i]['totalHashrate']}}</td>
+                <td>{{unserialize($minerData->data)[$i]['upTime']}}</td>
+            </tr>
+        @endfor
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <h2 class="title-1 m-b-25">Top countries</h2>
+                                <div class="au-card au-card--bg-blue au-card-top-countries m-b-40">
+                                    <div class="au-card-inner">
+                                        <div class="table-responsive">
+                                            <table class="table table-top-countries">
+                                                <tbody>
+                                                    <tr>
+                                                        <td>United States</td>
+                                                        <td class="text-right">$119,366.96</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Australia</td>
+                                                        <td class="text-right">$70,261.65</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>United Kingdom</td>
+                                                        <td class="text-right">$46,399.22</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Turkey</td>
+                                                        <td class="text-right">$35,364.90</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Germany</td>
+                                                        <td class="text-right">$20,366.96</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>France</td>
+                                                        <td class="text-right">$10,366.96</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Australia</td>
+                                                        <td class="text-right">$5,366.96</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Italy</td>
+                                                        <td class="text-right">$1639.32</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>        
+  <!--        <div class="table-responsive">
     <table class="table table-striped">
         <thead>
         <tr>
@@ -35,24 +101,12 @@
         </thead>
         <tbody>
 
-        @for($i=0;$i< count(unserialize($minerData->data));$i++)
-            <tr>
-                <td>{{unserialize($minerData->data)[$i]['ip']}}</td>
-                <td>{{unserialize($minerData->data)[$i]['minerName']}}</td>
-                <td>{{implode( ", ", unserialize($minerData->data)[$i]['temp2'])}}</td>
-                <td>{{implode( ", ", unserialize($minerData->data)[$i]['temp1'])}}</td>
-                <td>{{implode( ", ", unserialize($minerData->data)[$i]['fanSpeeds'])}}</td>
-                <td>{{unserialize($minerData->data)[$i]['totalHashrate']}}</td>
-                <td>{{unserialize($minerData->data)[$i]['upTime']}}</td>
-            </tr>
-        @endfor
+       
 
 
         </tbody>
     </table>
-  </div>
+  </div> -->
     @endif
-</div>
 
-</body>
-</html>
+@endsection

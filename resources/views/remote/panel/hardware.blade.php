99dad57d298eb35daa314b5831779bfd28fee6e9
Mohammad Vatandoost <mohamadvatandoost512@gmail.com>
2019-08-27 09:18:05 -0700

    add text to hardware page

--- resources/views/remote/panel/hardware.blade.php
+++ resources/views/remote/panel/hardware.blade.php
@@ -11,6 +11,8 @@
        <p>پیشنهاد ما این است، برای کاهش مصرف برق و پایدار بودن سیستم مدیریت ماینیگ سخت افزار بالا را تهیه کنید.</p>
        <p>تمام نرم افزار های مورد نیاز بر روی این سخت افزار نصب شده است، فقط کافی است کابل شبکه و برق را متصل کنید.</p>
        <p>هزینه سخت افزار : 600 هزار تومان است.</p>
+       <p>زمان دریافت برای مشتریان تهران 3 روز کاری و برای شهرستان ها 4 روز کاری است.</p>
+       <br/>
        <h4 class="text-center">برای ثبت سفارش فرم زیر را پر کنید.</h4>
        <form action="/action_page.php" class="was-validated" >
           <div class="form-group">
@@ -34,7 +36,32 @@
           </div>
        </form>
    </div>
-   <br/><br/><br/>
+   <br/><br/>
+   <div class="au-card text-right" style="direction: rtl;">
+     <h3 class="m-b-25 text-center">پیگیری سفارش ها</h3>
+     <p class="text-center m-t-2 m-b-2">سفارشی تا حالا ثبت نکردید.</p>
+     <div class="table-responsive">
+     <table class="table table-bordered">
+        <thead>
+          <tr>
+           <th>تاریخ</th>
+           <th>مبلغ</th>
+           <th>وضعیت</th>
+           <th>کد پیگیری</th>
+         </tr>
+        </thead>
+        <tbody>
+         <tr>
+           <td>1398/06/20</td>
+           <td>600000 تومان</td>
+           <td>در حال آماده سازی</td>
+           <td>1235468</td>
+        </tr>
+       </tbody>
+      </table>
+      </div> 
+   </div>
+   <br/><br/>
  @include('remote/scripts')
    <script type="text/javascript">
     

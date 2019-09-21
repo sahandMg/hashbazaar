06a311d3cdda514659eee548d00e88bbcd342fc8
sahandMg <sahand.mg.ne@gmail.com>
2019-09-21 23:30:58 +0330

    session check

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,6 +14,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
+        dd(\Auth::guard('user')->check());
         if (! $request->expectsJson()) {
             return route('login',['locale'=>session('locale')]);
         }
